6b08fe1d11bc42e3f3ed8106e6a668d8e3d3c604
iris <iriskurien@gmail.com>
2024-11-24 02:03:01 +0530

    quik fix

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call('auth.test')['user_id']
 
 @slack_event_adapter.on('message')
 def message(payLoad):
@@ -20,7 +21,8 @@ def message(payLoad):
     user_id = event.get('user')
     text = event.get('text')
 
-    client.chat_postMessage(channel=channel_id, text=text)
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id, text=text)
 
 if __name__ == "__main__":
     app.run(debug=True)
