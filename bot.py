590c5d70d78045aec8562ef7e506a943c005b7ef
iris <iriskurien@gmail.com>
2024-11-24 01:33:45 +0530

    accidentally made the ~annoying~ bot

--- bot.py
+++ bot.py
@@ -2,9 +2,25 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#test', text="hello tiny nation")
+
+@slack_event_adapter.on('message')
+def message(payLoad):
+    event = payLoad.get('event',{})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    client.chat_postMessage(channel=channel_id, text=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
